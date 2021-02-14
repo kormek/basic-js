64ff0fa6718996d6ef608b0566a8687965d83ec7
Vladimir <anikol200@gmail.com>
2021-02-14 22:07:00 +0300

    fix: playing cats was made with additional requirements

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,6 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
   let answer = 0;
+  if(arguments.length == 0 || matrix.length == 0) return 0;
   for(let i = 0;i<matrix.length;i++){
     for(let j = 0;i<matrix[i].length;j++){
       if(matrix[i][j]=="^^") answer++;
