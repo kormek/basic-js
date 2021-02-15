8df095e92b0719c3c7b1fae5bb4dd369807b099a
Vladimir <anikol200@gmail.com>
2021-02-15 16:28:01 +0300

    fix: task dream team was made

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(members == undefined || members == null || arguments.length == 0 || members.length <= 1  ) return false;
+  let answer =[];
+  for(i in members){
+    if(typeof(members[i]) == "string"){
+      answer.push(members[i].split('').filter(letter => letter !=' ')[0]);
+    }
+  }
+  if(answer.length <= 1) return false;
+  answer = answer.join('').toUpperCase();
+  answer = answer.split('');
+  answer.sort();
+  return answer.join('');
 };
