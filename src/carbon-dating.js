671ccdd092f79bfda2ea5d047390b8b7dceb774f
Vladimir <anikol200@gmail.com>
2021-02-15 11:42:54 +0300

    fix: carbon cats fixed and carbon date finished

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,16 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  if(arguments.length===0 || isNaN(Number(sampleActivity)) || typeof(sampleActivity) == "number" || Number(sampleActivity)===0 || typeof(sampleActivity)=== "object" || Number(sampleActivity)<=0 ||Number(sampleActivity)>15){
+    return false;
+  }
+  sampleActivity = parseInt(sampleActivity);
+  
+  let k=0.693/5730;
+
+
+  return Math.ceil(Math.log10(15/sampleActivity)/k);
+
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -4,9 +4,9 @@ module.exports = function countCats(matrix) {
   let answer = 0;
   if(arguments.length == 0 || matrix.length == 0) return 0;
   for(let i = 0;i<matrix.length;i++){
-    for(let j = 0;i<matrix[i].length;j++){
+    for(let j = 0;j<matrix[i].length;j++){
       if(matrix[i][j]=="^^") answer++;
     }
   }
   return answer;
-};
+}
