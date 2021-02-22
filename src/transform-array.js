d79802b66dc3090aa851cf98275c824ee9f26379
Vladimir <anikol200@gmail.com>
2021-02-22 15:24:15 +0300

    fix: transform-array done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(arguments.length == 0 || !Array.isArray(arr)) throw new Error();
+    let answer = [];
+    for(let i = 0;i<arr.length;i+=1){
+        
+        if(arr[i] == "--discard-next" && i<arr.length ){
+                answer.push(undefined);
+                i+=1;
+                
+        } else if(arr[i] == "--discard-prev" && i > 0 ){
+            answer.pop();
+        } else if(arr[i] == "--double-next" && i< arr.length ){
+            answer.push(arr[i+1]);
+        } else if(arr[i] == "--double-prev" && i>0 ){
+            answer.push(answer[answer.length-1]);
+        }
+        else if(arr[i] != "--discard-next" && arr[i] != "--discard-prev" && arr[i] != "--double-next" && arr[i] != "--double-prev"){
+            answer.push(arr[i]);
+        }
+    }
+    answer = answer.filter(x => x != undefined);
+    return answer;
 };
